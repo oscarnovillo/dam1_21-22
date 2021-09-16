6eec71a288acbefc5fa3f617d4a7623ebef23a61
oscar <asdasd>
2021-09-16 13:14:03 +0200

    asd

---
+++ PROGRAMACION/test/src/main/java/main/MiPrimerMain.java
@@ -0,0 +1,39 @@
+package main;
+
+
+import java.util.Scanner;
+
+public class MiPrimerMain {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//
+//        int i=80;
+//        double d = 8.8;
+//       i = 77;
+//        i = (int)d;
+//
+//        char c = 'o';
+//
+//        Integer i1 = i;
+//        i1 = 89;
+//        Double d2;
+//        String s;
+//        int suma = (int)(i+i1+10+d)*8;
+//        System.out.println("la suma es "+suma+1);
+//        System.out.print("Pon el nombre: ");
+//        System.out.println("dame el primer numero: ");
+        int numero = sc.nextInt();
+        int numero2 = sc.nextInt();
+
+        System.out.println(numero+numero2+ " .");
+
+//        System.out.println("Hola " + nombre);
+
+    }
+
+
+}
+
