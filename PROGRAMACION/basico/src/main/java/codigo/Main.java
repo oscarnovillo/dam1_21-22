6e3f123c73da4f5091067655f59d334e9d6fcad2
oscar <asdasd>
2021-09-23 09:31:21 +0200

    asdad

---
+++ PROGRAMACION/basico/src/main/java/codigo/Main.java
@@ -0,0 +1,25 @@
+package codigo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+
+        if ( (numero >= 5) && (numero <= 10)
+               ) {
+
+            System.out.println("por el **** te la inco");
+                System.out.println("y tu madre pega un brinco");
+        }
+        else
+        {
+            System.out.println("pringado que no sabes rimas");
+        }
+
+        System.out.println("fin");
+    }
+}
---
+++ PROGRAMACION/basico/src/main/java/codigo/MainSwitch.java
@@ -0,0 +1,32 @@
+package codigo;
+
+public class MainSwitch {
+
+
+    public static void main(String[] args) {
+
+        int kaka = 5;
+        String nombre = "PEPE";
+
+        switch(kaka){
+            case 2: {
+                System.out.println("cuatro");
+            }
+                break;
+            case 3:
+                System.out.println("cinco");
+                break;
+            case 5:
+                System.out.println("seis");
+                break;
+            default:
+                System.out.println("no encontrado");
+
+        }
+
+
+
+
+
+    }
+}
