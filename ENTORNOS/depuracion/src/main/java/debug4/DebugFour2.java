b78b45cf9574c47c1f9d225ccfb4771fe384b28b
pepe <oscar.novillo@gmail.com>
2021-10-24 13:09:35 +0200

    c

---
+++ ENTORNOS/depuracion/src/main/java/debug4/DebugFour2.java
@@ -0,0 +1,15 @@
+package debug4;
+
+// Some circle statistics
+public class FixDebugFor2
+{
+   public static void main(String args[])
+   {
+      int radius = 12.6;
+      System.out.println("Circle statistics");
+      double area = Math.PI * radius * radius;
+      System.out.println("area is " + area);
+      double diameter = 2 * diameter;
+      System.out.println("diameter is " + diameter)
+   }
+}
