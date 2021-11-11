eb1272c50610939a620838da29293a0a708d9ebf
oscar <adsadf>
2021-11-11 09:54:36 +0100

    adasd

--- PROGRAMACION/arrays/src/main/java/cartas/ui/Cartas.java
+++ PROGRAMACION/arrays/src/main/java/cartas/ui/Cartas.java
@@ -14,6 +14,35 @@ public class Cartas {
         int []cartasFrancesas = serviciosCartas.generarBaraja(52,13);
 
 
+        int numeroJugadores = 4;
+        int indiceBaraja = 0;
+        int indiceJugadorActual = 0;
+
+        int [][]cartasJugadores = new int[numeroJugadores][10];
+        int []sumaJugadores = new int[numeroJugadores];
+
+
+
+
+        for (int i = 0; i < numeroJugadores; i++) {
+
+            cartasJugadores[i][indiceJugadorActual] = cartas[indiceBaraja];
+            indiceBaraja++;
+
+        }
+
+        boolean carta = true;
+        for (int i = 0; i < numeroJugadores; i++) {
+            indiceJugadorActual = 1;
+            //quieres carta
+            while ( carta) {
+                cartasJugadores[i][indiceJugadorActual] = cartas[indiceBaraja];
+                indiceBaraja++;
+
+                indiceJugadorActual++;
+            }
+
+        }
 
 
 
