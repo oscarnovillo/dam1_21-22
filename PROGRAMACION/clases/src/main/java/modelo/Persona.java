cb09640a489cde32cfe47a1463c9adcfd8694edd
oscar <adsadf>
2021-12-02 12:50:45 +0100

    asdad

---
+++ PROGRAMACION/clases/src/main/java/modelo/Persona.java
@@ -0,0 +1,73 @@
+package modelo;
+
+// POJO
+
+public class Persona {
+
+    private String nombre;
+    private String dni;
+    private String apellido;
+    private boolean vivo;
+    private int asesinados;
+
+    private Persona()
+    {
+        asesinados = 0;
+        vivo =true;
+    }
+
+    public Persona(String nombre, String dni, String apellido) {
+        this();
+        this.nombre = nombre;
+        this.dni = dni;
+        this.apellido = apellido;
+
+    }
+
+    public Persona(String nombre, String apellido) {
+        this(nombre,"hj",apellido);
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nombre='" + nombre + "'" +
+                ", dni='" + dni + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", vivo='" + vivo + '\'' +
+                '}';
+    }
+}
---
+++ PROGRAMACION/clases/src/main/java/servicios/ServiciosPersonas.java
@@ -0,0 +1,14 @@
+package servicios;
+
+import dao.DaoPersona;
+import modelo.Persona;
+
+public class ServiciosPersonas {
+
+
+
+    public void matar(Persona asesino, Persona victima){
+        DaoPersona dao = new DaoPersona();
+
+    }
+}
