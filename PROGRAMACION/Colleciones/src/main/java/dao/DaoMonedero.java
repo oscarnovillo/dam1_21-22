288a4c6dcef040f9074a6ee75e02abccf28d13e2
oscar <adsadf>
2022-02-08 13:03:18 +0100

    awd

--- PROGRAMACION/Colleciones/src/main/java/dao/DaoMonedero.java
+++ PROGRAMACION/Colleciones/src/main/java/dao/DaoMonedero.java
@@ -1,11 +1,34 @@
 package dao;
 
-import java.util.List;
+import modelo.Cliente;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class DaoMonedero {
 
 
 
+
+    public void addDinerito(String dni,String monederoName,double cantidad)
+    {
+        Cliente c = BD.clientes.get(dni);
+
+
+        c.getMonederos().forEach(monedero -> {
+            if (monedero.getNombre().equals(monederoName)) {
+                monedero.setDinero(monedero.getDinero() + cantidad);
+            }
+        });
+
+
+//        c.getMonederos().stream()
+//                .filter(monedero -> monedero.getNombre().equals(monederoName))
+//                .findFirst().get().setDinero();
+
+
+
+
+    }
 
 
 
--- PROGRAMACION/Colleciones/src/main/java/modelo/Monedero.java
+++ PROGRAMACION/Colleciones/src/main/java/modelo/Monedero.java
@@ -1,8 +1,13 @@
 package modelo;
 
+
+import lombok.Data;
+
+@Data
 public class Monedero {
 
     private String nombre;
     private double dinero;
+
 
 }
